dae62d40ab96b794b10e5be0a859cf27acf03d7d
Michael3573 <126564353+Michael3573@users.noreply.github.com>
2023-08-14 12:56:16 +0200

    commit-3

--- src/js/refs.js
+++
@@ -1,6 +0,0 @@
-export default {
-  formEl: document.querySelector('.search-form'),
-  galleryWrapperEl: document.querySelector('.gallery'),
-  btnEl: document.querySelector('.load-more'),
-  spanEl: document.querySelector('.js-span'),
-};
